62bd607c955a11e5cc0ab5bf997852973f4d4f28
getsadzeg <getsadzeguram@gmail.com>
2016-01-07 23:01:42 +0400

    add nested statements

--- src/decision_making.py
+++ src/decision_making.py
@@ -1,3 +1,9 @@
 var = 99; #Try assigning value 100. It will be true
-if(var == 100) : print "Yeah, hundred equals 100";
-else : print "Nope. num doesn't equals 100";
+if(var == 100) : print "Yeah, var equals 100";
+else : print "Nope. var doesn't equals 100";
+
+if(var < 30):
+	print "var is less than 30";
+
+elif(var > 30): # 'elif' is absolutely equivalent of 'else if'. I didn't know about it.
+	print "var is greater than 30";
