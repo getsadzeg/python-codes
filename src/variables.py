294ab64b94dbe804610e7f8f7524f32b5aa62633
getsadzeg <getsadzeguram@gmail.com>
2016-01-07 21:54:31 +0400

    add arithmetic ops

--- src/variables.py
+++ src/variables.py
@@ -1,6 +1,12 @@
 num = 4; # will be integer variable
 str = "Hello, String."; # will be string variable
 flnum = 4.0; # will be float num
-print num;
-print str;
-print flnum;
+print "First condition for variable num:", num;
+print "First condition for variable str:", str;
+print "First condition for variable flnum:", flnum;
+num+=2;
+str+=" Love you";
+flnum-=2.0;
+print "Second condition for variable num:", num;
+print "Second condition for variable str:", str;
+print "Second condition for variable flnum", flnum;
