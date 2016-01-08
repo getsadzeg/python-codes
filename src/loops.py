77c8d4346b28fc204fc91556df582cc5545770ae
getsadzeg <getsadzeguram@gmail.com>
2016-01-08 15:53:22 +0400

    add while loop(s)

---
+++ src/loops.py
@@ -0,0 +1,10 @@
+counter = 0;
+while(counter < 10): # while loop
+	counter+=1;
+	print "Now counter is ", counter;
+
+
+print "out of loop" # indentation here
+
+while(counter): # Ok. Just infinite loop.
+	print "LOL";
