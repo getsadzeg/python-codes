c76328b32a4431f0a2b0ee2f7ff649147dcd744d
getsadzeg <getsadzeguram@gmail.com>
2016-01-08 18:14:11 +0400

    add for loop(s)

--- src/loops.py
+++ src/loops.py
@@ -6,5 +6,12 @@ while(counter < 10): # while loop
 
 print "out of loop" # indentation here
 
-while(counter): # Ok. Just infinite loop.
-	print "LOL";
+"""while(counter): 
+# Ok. Just infinite loop.
+	print "LOL";"""
+
+for letter in "Python":
+	print letter, "\n"; #Prints every letter in referenced word
+
+for i in range(1,10):
+	print i; #Prints numbers in range of (1,10)
