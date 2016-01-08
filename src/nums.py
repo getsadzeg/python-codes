8e9e6c81ceee31e13278414d8ec7a43893fc9e66
getsadzeg <getsadzeguram@gmail.com>
2016-01-08 21:55:27 +0400

    add random

--- src/nums.py
+++ src/nums.py
@@ -1,4 +1,5 @@
 import math as m #Import math library and use it as 'm'.
+import random as r
 n = 10;
 print n;
 """del(n); # Deletes object's reference.
@@ -9,3 +10,4 @@ print n;""" # compilation error, because it's undefined after calling 'del'
 print abs(n); 
 print pow(n, 2); 
 print m.sqrt(n);
+print "random calling: ", r.random();
