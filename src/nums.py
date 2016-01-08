83dc1bba081815da3501877c5e0629a78a2711b0
getsadzeg <getsadzeguram@gmail.com>
2016-01-08 21:37:27 +0400

    some numbers' functions demonstration

---
+++ src/nums.py
@@ -0,0 +1,11 @@
+import math as m #Import math library and use it as 'm'.
+n = 10;
+print n;
+"""del(n); # Deletes object's reference.
+print n;""" # compilation error, because it's undefined after calling 'del'
+
+# Demonstrate some functions
+
+print abs(n); 
+print pow(n, 2); 
+print m.sqrt(n);
