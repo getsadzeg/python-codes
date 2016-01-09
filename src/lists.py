c31b8e0d5fa74e6ba3f2959fefe83e2166f62229
getsadzeg <getsadzeguram@gmail.com>
2016-01-09 21:56:09 +0400

    playing with list

---
+++ src/lists.py
@@ -0,0 +1,6 @@
+langlist = ["cpp", "c", "java", "python"]
+print "first element of langlist:", langlist[0];
+langlist[0] = "C#"; # Updating list element
+print "after modification:", langlist[0];
+for element in langlist:
+	print element; # Prints each element of list
