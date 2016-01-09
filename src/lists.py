e9aab139c835cce09e02878ac51e0529d72f600f
getsadzeg <getsadzeguram@gmail.com>
2016-01-09 22:08:27 +0400

    add len and del functions usage example

--- src/lists.py
+++ src/lists.py
@@ -2,5 +2,8 @@ langlist = ["cpp", "c", "java", "python"]
 print "first element of langlist:", langlist[0];
 langlist[0] = "C#"; # Updating list element
 print "after modification:", langlist[0];
+size = len(langlist);
+print "langlist's size is",size;
+del langlist[1]; # del calling
 for element in langlist:
-	print element; # Prints each element of list
+	print element; # Prints each element of list except second element because of 'del' calling
