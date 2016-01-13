e4477966dddf360f0a2f4296072549ce6fea491e
getsadzeg <getsadzeguram@gmail.com>
2016-01-13 21:38:53 +0400

    hello to passing args and default args

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,28 @@
 def greeting(Name):
     "This function greets to person with Name."  # Docs
-    print "Hello", Name
+    return "Hello " + Name
+
+print greeting("John")
+
+
+def plus(x, y):
+    #  x variable won't change outside the function because of immutability
+    x += 4
+    y = 8
+    print "Inside x:", x
+    print "Inside y:", y  # will be 8, reference is being overwritten
     return
 
-greeting("John")
+x = 5  # won't be 9
+y = 4
+plus(x, y)
+print "Outside x:", x
+print "Outside y:", y  # won't be change
+
+
+def setbaby(name, age=0):
+    print "name:", name
+    print "age:", age
+    return
+
+setbaby(name="zuck") # we give no argument of age, so it will be 0
